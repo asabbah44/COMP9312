a46bdbc5522e706f9a50b09f1c63b0a0c9afe70c
asabbah44 <43098459+asabbah44@users.noreply.github.com>
2022-06-16 18:28:30 +0300

    Splite subsets datasets

--- comp9312/prepareData/Sub1/SplitData.py
+++ comp9312/prepareData/Sub1/SplitData.py
@@ -1,21 +1,34 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# All data only spilte
 
 # load the iris dataset and get X and Y data
-
-data = pd.read_csv("TD2.csv")
+ #["DESIGN", "IMPLEMENTATION", "TEST","DEFECT","DOCUMENTATION"],
+data = pd.read_csv("TDSub1.csv")
 df = pd.DataFrame(data)
+df_skip_doc=df[df.label == 'DOCUMENTATION']
+df_skip_test=df[df.label == 'TEST']
 
-train,test = train_test_split(df, test_size=0.20, random_state=0)
+df_in_design=df[df.label == 'DESIGN']
+df_in_implementation=df[df.label == 'IMPLEMENTATION']
+df_in_defect=df[df.label == 'DEFECT']
 
-train,val = train_test_split(train, test_size=0.10, random_state=0)
+IncludeDesign,ExcludeDes = train_test_split(df_in_design, test_size=0.90, random_state=0)
+IncludeImplementation,ExcludeImp = train_test_split(df_in_implementation, test_size=0.60, random_state=0)
+IncludeDef,ExcludeDef = train_test_split(df_in_defect, test_size=0.50, random_state=0)
 
 
-train.to_csv('train.csv',index=False)
-test.to_csv('test.csv',index=False)
-val.to_csv('val.csv',index=False)
+df=pd.concat([IncludeDesign, IncludeImplementation,IncludeDef, df_skip_doc, df_skip_test])
+print(df.count())
+
+# train,test = train_test_split(df, test_size=0.20, random_state=0)
+#
+# train,val = train_test_split(train, test_size=0.10, random_state=0)
+#
+#
+# train.to_csv('train.csv',index=False)
+# test.to_csv('test.csv',index=False)
+# val.to_csv('val.csv',index=False)
 
 # print(X_train.head())
 # print(X_test)
--- comp9312/prepareData/Sub2/SplitData.py
+++ comp9312/prepareData/Sub2/SplitData.py
@@ -5,8 +5,22 @@ from sklearn.model_selection import train_test_split
 
 # load the iris dataset and get X and Y data
 
-data = pd.read_csv("TD2.csv")
+data = pd.read_csv("TDSub2.csv")
 df = pd.DataFrame(data)
+df_skip_doc=df[df.label == 'DOCUMENTATION']
+df_skip_test=df[df.label == 'TEST']
+
+df_in_design=df[df.label == 'DESIGN']
+df_in_implementation=df[df.label == 'IMPLEMENTATION']
+df_in_defect=df[df.label == 'DEFECT']
+
+IncludeDesign,ExcludeDes = train_test_split(df_in_design, test_size=0.70, random_state=0)
+IncludeImplementation,ExcludeImp = train_test_split(df_in_implementation, test_size=0.40, random_state=0)
+IncludeDef,ExcludeDef = train_test_split(df_in_defect, test_size=0.30, random_state=0)
+
+
+df=pd.concat([IncludeDesign, IncludeImplementation,IncludeDef, df_skip_doc, df_skip_test])
+print(df.count())
 
 train,test = train_test_split(df, test_size=0.20, random_state=0)
 
@@ -16,6 +30,7 @@ train,val = train_test_split(train, test_size=0.10, random_state=0)
 train.to_csv('train.csv',index=False)
 test.to_csv('test.csv',index=False)
 val.to_csv('val.csv',index=False)
+
 
 # print(X_train.head())
 # print(X_test)
