bdbab12e8a7108572804969290f25a57f403dc12
asabbah44 <43098459+asabbah44@users.noreply.github.com>
2022-07-20 16:30:19 +0300

    commit

--- comp9312/prepareData/Oversample.py
+++ comp9312/prepareData/Oversample.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
-
 # load the iris dataset and get X and Y data
 
 data = pd.read_csv("train.csv")
@@ -10,13 +8,13 @@ df = pd.DataFrame(data)
 
 train, skip = train_test_split(df, test_size=0.000001, random_state=0)
 
-#train,val = train_test_split(train, test_size=0.10, random_state=0)
+# train,val = train_test_split(train, test_size=0.10, random_state=0)
 
-print(f"Training target statistics: ",train.count())
-from imblearn.over_sampling import RandomOverSampler,SMOTEN
+print(f"Training target statistics: ", train.count())
+from imblearn.over_sampling import RandomOverSampler, SMOTEN
 
-x=pd.DataFrame(train["text"])
-y=pd.DataFrame(train["label"])
+x = pd.DataFrame(train["text"])
+y = pd.DataFrame(train["label"])
 
 over_sampler = RandomOverSampler(random_state=42)
 X_res, y_res = over_sampler.fit_resample(x, y)
@@ -24,15 +22,16 @@ X_res, y_res = over_sampler.fit_resample(x, y)
 SMOTEN_over_sampler = SMOTEN(random_state=42)
 X_res_smo, y_res_som = SMOTEN_over_sampler.fit_resample(x, y)
 
-print(f"Training target Random: ",X_res.count())
-print(f"Training target  SMOTEN : ",X_res_smo.count())
+print(f"Training target Random: ", X_res.count())
+print(f"Training target  SMOTEN : ", X_res_smo.count())
 
-trainOver=pd.concat([X_res, y_res], axis=1)
-trainSMOTEN=pd.concat([X_res_smo, y_res_som], axis=1)
+trainOver = pd.concat([X_res, y_res], axis=1)
+trainSMOTEN = pd.concat([X_res_smo, y_res_som], axis=1)
 print(trainOver.head())
+print((trainOver["label"] == "DESIGN").count())
 
-trainOver.to_csv('R_overtrain.csv',index=False)
-trainSMOTEN.to_csv('SMOTEN_train.csv',index=False)
+# trainOver.to_csv('R_overtrain.csv',index=False)
+# trainSMOTEN.to_csv('SMOTEN_train.csv',index=False)
 # val.to_csv('val.csv',index=False)
 
 # print(X_train.head())
@@ -42,6 +41,3 @@ trainSMOTEN.to_csv('SMOTEN_train.csv',index=False)
 # print(X_train.shape)
 # print(X_test.shape)
 # print(X_valid.shape)
-
-
-
--- comp9312/prepareData/Sub3/SplitData.py
+++ comp9312/prepareData/Sub3/SplitData.py
@@ -2,16 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-
-# load the iris dataset and get X and Y data
-
 data = pd.read_csv("TDSub1.csv")
 df = pd.DataFrame(data)
 
 train,test = train_test_split(df, test_size=0.20, random_state=0)
 
 train,val = train_test_split(train, test_size=0.10, random_state=0)
-
 
 train.to_csv('train.csv',index=False)
 test.to_csv('test.csv',index=False)
